dbb63508fde19d3e290b254748a1257e037637e4
Jianmin Wei <jianminwei1@gmail.com>
2023-03-06 12:38:09 -0500

    Removed some not-needed code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,6 @@ import {eventDispatcher, store} from './store';
 import {ActionTypes} from './store/actions';
 import {Note} from './note-card/note-card.component';
 
-//you can define a infile interface, pretty cool.
-interface Food {
-  value: string;
-  viewValue: string;
-}
-
 
 @Component({
   selector: 'app-root',
@@ -27,38 +21,11 @@ export class AppComponent implements OnInit{
   }
 
 
-
   ngOnInit() {
     eventDispatcher.next({
       type: ActionTypes.GET_NOTES,
       payload: this.notes
     });
   }
-
-  /**
-   * Below section is for material UI stuff.
-   */
-  title = 'material-demo';
-  notifications=1;
-  sidenavOpened = false;
-  tabIndex:number=0;
-
-  log(state:string) {
-    console.log(state);
-  }
-
-  tabSelectedIndexChange(selectedTabIndex:any) {
-    this.tabIndex = selectedTabIndex;
-
-  }
-
-  // for Mat form select
-  foods: Food[] = [
-    {value: 'steak-0', viewValue: 'Steak'},
-    {value: 'pizza-1', viewValue: 'Pizza'},
-    {value: 'tacos-2', viewValue: 'Tacos'},
-  ];
-
-  selectedValue:string = "";
 
 }
